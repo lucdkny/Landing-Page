8fdbcee644c8d56aabc56f4be906ca3cbaaaf9f3
TranQuangSon <sontqph19321@fpt.edu.vn>
2023-11-09 22:14:36 +0700

    Update Global Footer

--- src/modules/site-footer/site-footer.js
+++ src/modules/site-footer/site-footer.js
@@ -6,6 +6,18 @@ class SiteFooter extends HTMLElement {
     console.log(this.button)
   }
 }
-
 customElements.define('site-footer', SiteFooter);
+
+//Scroll to top
+
+const goTopBtn = document.querySelector('.footer__button-text');
+
+goTopBtn.addEventListener('click', () => {
+  //"click" cho phần tử goTopBtn. Khi phần tử này được nhấp chuột, một hàm callback sẽ được gọi.
+  
+  window.scrollTo({
+    top: 0,
+    behavior:"smooth"
+  })
+})
 
